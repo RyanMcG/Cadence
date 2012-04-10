a0cea7c604c91d29c593d09bf20d8385ce25f003
Ryan McGowan <ryan@ryanmcg.com>
2012-04-10 00:17:54 -0400

    Implementing noir-cljs.

--- project.clj
+++ project.clj
@@ -2,13 +2,6 @@
             :description "Use pattern recognition to match users with Cadence.js
                          output."
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir-cljs "0.3.0"]
                            [noir "1.2.0"]]
-            :dev-dependencies [[lein-cljsbuild "0.0.13"]] ; cljsbuild plugin
-            :cljsbuild {
-                        :source-path "src-cljs"
-                        :compiler {
-                         :output-to "resources/public/js/cljs.js"
-                         :optimizations :simple
-                         :pretty-print true } }
-            :repl-init cadence.repl
-            :main cadence.server)
+            :main ^{:skip-aot true} cadence.server)
--- src/cadence/server.clj
+++ src/cadence/server.clj
@@ -1,11 +1,15 @@
 (ns cadence.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [noir.cljs.core]))
 
 (server/load-views "src/cadence/views/")
+
+(def cljs-options {:advanced {}})
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (noir.cljs.core/start mode cljs-options)
     (server/start port {:mode mode
                         :ns 'cadence})))
 
