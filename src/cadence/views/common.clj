11dfec621d36cffa247e19a7b5016e1ae4710ded
Ryan McGowan <ryan@ryanmcg.com>
2012-04-14 03:54:14 -0400

    Started creating default layout.

--- src/cadence/views/common.clj
+++ src/cadence/views/common.clj
@@ -1,15 +1,42 @@
 (ns cadence.views.common
   (:use noir.core
         hiccup.core
-        hiccup.page-helpers))
+        hiccup.page-helpers
+        cadence.views.landing))
 
-(defpartial layout [& content]
+(defpartial base-layout [& content]
             (html5
               [:head
                [:title "Cadence"]
+               ; Necessary for Twitter Boostrap
+               [:meta {:name "viewport"
+                       :content "width=device-width, initial-scale=1.0"}]
+               ; TODO Add Twitter boostrap
+               ; TODO Remove this?
                (include-css "/css/reset.css")
-               (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")]
+               ; TODO Remove this?
+               (include-js
+                       "https://ajax.googleapis.com/ajax/libs/"
+                       "jquery/1.7.2/jquery.min.js")]
               [:body
-               [:div#wrapper
-                content]]
+               content]
               (include-js "/js/bootstrap.js")))
+
+(defpartial user-links []
+            [:a {:href (url-for signup)} "login / sign up"])
+
+(defpartial layout [& content]
+            (base-layout
+              [:header
+               [:div.wrapper
+                [:img#logo {:src "/img/cadence.png"}]
+                [:div#title "Cadence"]
+                [:div#user-links (user-links)]]]
+              [:div#main.wrapper content]
+              [:footer
+               [:div.wrapper [:div.section
+                              [:ul
+                               [:li [:a {:href (url-for about)} "About"]]
+                               ]]]]))
+
+
--- src/cadence/views/landing.clj
+++ src/cadence/views/landing.clj
@@ -5,7 +5,17 @@
         hiccup.core
         hiccup.page-helpers))
 
-(defpage "/" []
+(defpage root "/" []
          (common/layout
            [:h1 "Cadence"]
            [:p "Welcome to cadence"]))
+
+(defpage login "/signup" []
+         (common/layout
+           [:h1 "Login or Sign Up"]
+           [:p "WELCOME TO THE LOGIN PAGE!"]))
+
+(defpage about "/about" []
+         (common/layout
+           [:h1 "About"]
+           [:p "derpderpderp"]))
