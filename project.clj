c7d695639ce235867c9e03f14b5486f07db1013d
Ryan McGowan <ryan@ryanmcg.com>
2012-04-21 03:17:59 -0400

    Updgraded noir to 1.2.1.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
             :description "Use pattern recognition to match users with Cadence.js
                          output."
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.0"]]
+                           [noir "1.2.1"]]
             :main cadence.server)
