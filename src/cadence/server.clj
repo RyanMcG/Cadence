b98b2b1d951a1dfc7ed110acec8a90635a87d0e9
Ryan McGowan <ryan@ryanmcg.com>
2012-04-25 03:08:40 -0400

    Added simple ring middleware to always redirect to https.

--- src/cadence/server.clj
+++ src/cadence/server.clj
@@ -3,11 +3,25 @@
 
 (server/load-views "src/cadence/views/")
 
+(defn https-url [request-url]
+  (str "https://"
+       (:server-name request-url)
+       (let [port (:server-port request-url)]
+         (if (nil?  port) (str ":" port)))
+       (:uri request-url)))
+
+(defn require-https [handler]
+  (fn [request]
+    (if (= (:scheme request) :http)
+      (ring.util.response/redirect (https-url request))
+      (handler request))))
+
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "5010"))
-        url "cadence.ryanmcg.com"]
+        port (Integer. (get (System/getenv) "PORT" "5000"))
+        url "cadence.herokuapp.com"]
+    (if (not= mode :dev) (server/add-middleware require-https))
     (server/start port {:mode mode
                         ;:base-url (if (= mode :dev) (str "test" url))
                         :ns 'cadence})))
-
