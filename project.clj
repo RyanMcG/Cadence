6f9f95e08ca429db8135a2dea5c5e896ba5ce40f
Ryan McGowan <ryan@ryanmcg.com>
2012-04-24 21:05:30 -0400

    Added cemeric/friend dependency.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject cadence "0.1.0-SNAPSHOT"
+(defproject cadence "0.2.0-SNAPSHOT"
             :description "Use pattern recognition to match users with Cadence.js
                          output."
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [com.cemerick/friend "0.0.7"]
                            [noir "1.2.1"]]
             :main cadence.server)
