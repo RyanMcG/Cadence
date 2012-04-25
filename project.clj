94d9f25272e13c9426feda30d0f4c9c41b8063cd
Ryan McGowan <ryan@ryanmcg.com>
2012-04-25 03:11:00 -0400

    Added monger dependencies.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject cadence "0.2.0-SNAPSHOT"
             :description "Use pattern recognition to match users with Cadence.js
                          output."
+            :min-lein-version "2.0.0"
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [com.novemberain/monger "1.0.0-beta4"]
                            [com.cemerick/friend "0.0.7"]
                            [noir "1.2.1"]]
             :main cadence.server)
