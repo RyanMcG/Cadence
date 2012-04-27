24c877e1831134dbd3c12284b16bbb788c3ab408
Ryan McGowan <ryan@ryanmcg.com>
2012-04-27 13:51:08 -0400

    Added login page stubs.

--- src/cadence/views/landing.clj
+++ src/cadence/views/landing.clj
@@ -1,5 +1,7 @@
 (ns cadence.views.landing
-  (:require [cadence.views.common :as common])
+  (:require [cadence.views.common :as common]
+            [noir.response :as response]
+            [noir.cookies :as cook])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
@@ -9,9 +11,21 @@
            [:h1 "Cadence"]
            [:p "Welcome to cadence"]))
 
+(defpage login "/login" []
+         (common/layout
+           [:h1 "Login"]
+           [:p "WELCOME TO THE LOGIN PAGE!"]
+           (:form [:post "/login"]
+                    [:input "Username"]
+                    [:input "Password"]
+                    [:input "Login"])))
+
+(defpage login-check [:post "/login"] []
+         (response/empty {:hello "world"}))
+
 (defpage signup "/signup" []
          (common/layout
-           [:h1 "Login or Sign Up"]
+           [:h1 "Login"]
            [:p "WELCOME TO THE LOGIN PAGE!"]))
 
 (defpage about "/about" []
