cc15dd4823e46379ba880c3ca0abc0c3f7772d4e
Ryan McGowan <ryan@ryanmcg.com>
2012-05-02 03:04:33 -0400

    Added more logic for specifying base-url.

--- src/cadence/server.clj
+++ src/cadence/server.clj
@@ -18,13 +18,14 @@
       (ring.util.response/redirect (https-url request))
       (handler request))))
 
-
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "5000"))
         url "cadence.herokuapp.com"]
     (if (not= mode :dev) (server/add-middleware require-https))
     (server/add-middleware wrap-gzip)
-    (server/start port {:mode mode
-                        ;:base-url (if (= mode :dev) (str "test" url))
-                        :ns 'cadence})))
+    (server/start port (let [opts {:mode mode
+                                   :ns 'cadence}]
+                         (if (not= mode :dev)
+                           (assoc opts :base-url url)
+                           opts)))))
