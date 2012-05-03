b0d5c3cedc699fae40c342d3f1584c3ede1ede82
Ryan McGowan <ryan@ryanmcg.com>
2012-05-03 03:23:47 -0400

    Use form helpers to create login form.

--- src/cadence/views/landing.clj
+++ src/cadence/views/landing.clj
@@ -4,21 +4,31 @@
             [noir.cookies :as cook])
   (:use noir.core
         hiccup.core
-        hiccup.page-helpers))
+        hiccup.page-helpers
+        hiccup.form-helpers))
 
 (defpage root "/" []
   (common/layout
     [:h1 "Cadence"]
     [:p "Welcome to cadence"]))
 
+(defn alabel [name] (label name name))
+
+(defpartial form-with-labels
+  "Returns labels and text fields for each name in the given collection."
+  [coll]
+  (for [field coll]
+    (alabel field)
+    ((if ((.toLower field) == "password") password-field text-field) field)))
+
 (defpage login "/login" []
   (common/layout
     [:h1 "Login"]
     [:p "WELCOME TO THE LOGIN PAGE!"]
-    [:form [:post "/login"]
-     [:input "Username"]
-     [:input "Password"]
-     [:input "Login"]]))
+    (with-group "login"
+                (form-to [:post "/login"]
+                         (form-with-labels ["Username" "Password"])
+                         (submit-button "Login")))))
 
 (defpage login-check [:post "/login"] []
   (response/empty {:hello "world"}))
