a6fcbcf7fb02b1df931bc9bb598ddf736e9c9549
Ryan McGowan <ryan@ryanmcg.com>
2012-05-05 02:24:28 -0400

    Improved login and sign up forms.

--- src/cadence/views/landing.clj
+++ src/cadence/views/landing.clj
@@ -1,42 +1,46 @@
 (ns cadence.views.landing
   (:require [cadence.views.common :as common]
-            [noir.response :as response]
-            [noir.cookies :as cook])
+            [noir.response :as response])
   (:use noir.core
         hiccup.core
-        hiccup.page-helpers
-        hiccup.form-helpers))
+        hiccup.page-helpers))
 
 (defpage root "/" []
   (common/layout
     [:h1 "Cadence"]
     [:p "Welcome to cadence"]))
 
-(defn alabel [name] (label name name))
-
-(defpartial form-with-labels
-  "Returns labels and text fields for each name in the given collection."
-  [coll]
-  (for [field coll]
-    (alabel field)
-    ((if ((.toLower field) == "password") password-field text-field) field)))
-
 (defpage login "/login" []
   (common/layout
     [:h1 "Login"]
-    [:p "WELCOME TO THE LOGIN PAGE!"]
-    (with-group "login"
-                (form-to [:post "/login"]
-                         (form-with-labels ["Username" "Password"])
-                         (submit-button "Login")))))
+    [:form#login.well.form-inline {:action "/login" :method "POST"}
+     [:input.input-small
+      {:type "text" :name "username" :placeholder "Username"}] " "
+     [:input.input-small
+      {:type "password" :name "password" :placeholder "Password"}] " "
+     [:button.btn {:type "submit"} "Log In"]]))
 
 (defpage login-check [:post "/login"] []
-  (response/empty {:hello "world"}))
+  (response/json {:hello "world"}))
 
 (defpage signup "/signup" []
   (common/layout
-    [:h1 "Login"]
-    [:p "WELCOME TO THE LOGIN PAGE!"]))
+    [:h1 "Sign Up"]
+    [:form#login.well.form-horizontal {:action "/signup" :method "POST"}
+     [:fieldset
+      [:div.control-group
+      [:label.control-label {:for "username"} "Username: "]
+      [:div.controls [:input {:type "text" :name "username" :placeholder ""}]]]
+      [:div.control-group
+      [:label.control-label {:for "email"} "Email: "]
+      [:div.controls [:input {:type "email" :name "email" :placeholder "you@youremail.com"}]]]
+      [:div.control-group
+      [:label.control-label {:for "password"} "Password: "]
+      [:div.controls [:input {:type "password" :name "password"}]]]
+      [:div.form-actions [:button.btn.btn-primary {:type "submit"} "Sign In"]]]]))
+
+(defpage signup-check [:post "/signup"] []
+  (response/json {:herp "derp"}))
 
 (defpage about "/about" []
   (common/layout
