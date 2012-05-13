ff6fa1ea9b9228e6a23e6e6bd103b39bb1e7d010
Ryan McGowan <ryan@ryanmcg.com>
2012-05-13 17:19:01 -0400

    Adds cadence.config to read system configuration.
    `cadence.config` has a `read-config` function which first attempts to
    find the designated variable in the system environment variables and
    falls back on user created config file along the resource path called
    `config.clj`.

--- src/cadence/config.clj
+++ src/cadence/config.clj
@@ -1,8 +1,24 @@
-(ns cadence.config)
+(ns cadence.config
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io])
+  (:import [java.net URL]))
 
-(def config
-  {:webapp {}
-   :storage {:type :mongodb
-             :uri (System/getenv "MONGOHQ_URL")
-             :username "cadence"
-             :password "secret"}})
+(def config-from-file
+  (load-file (.getPath (io/resource "config.clj"))))
+
+(defn read-config [config-var]
+  (or (System/getenv config-var) ""
+     (find config-from-file
+             (keyword (string/replace
+                        (string/lower-case config-var)
+                        "_" "-")))))
+
+(def storage {:uri (read-config "MONGOHQ_URL")
+              :db-name (read-config "MONGO_DB_NAME")
+              :username (read-config "MONGO_USERNAME")
+              :password (read-config "MONGO_PASSWORD")})
+
+(def tokens
+  {:twitter
+   {:token (read-config "TWITTER_OAUTH_TOKEN")
+    :secret-token (read-config "TWITTER_OAUTH_SECRET_TOKEN")}})
