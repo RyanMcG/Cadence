2d1952771c80d1e195eb708ca4b48b760be5bdb0
Ryan McGowan <ryan@ryanmcg.com>
2012-05-15 01:37:40 -0400

    Updates add and get for user in model.

--- src/cadence/model.clj
+++ src/cadence/model.clj
@@ -1,22 +1,27 @@
 (ns cadence.model
   (:require [monger.core :as mg]
             [monger.collection :as mc]
-            [cadence.model.validators :as is-valid]))
+            [cadence.model.validators :as is-valid])
+  (:use clojure.walk
+        [cemerick.friend.credentials :only [hash-bcrypt]]))
+
 
 (defn connect [connection-info]
   (if (:uri connection-info)
     (mg/connect-via-uri! (:uri connection-info))
     (mg/connect!))
   (let [db-name (:db-name connection-info)]
-    (mg/authenticate db-name (:username connection-info) (into-array Character/TYPE (:password connection-info)))
+    (mg/authenticate db-name
+                     (:username connection-info)
+                     (into-array Character/TYPE (:password connection-info)))
     (mg/set-db! (mg/get-db db-name))))
 
 (defn get-user [username]
-  (mc/find "users" {:username username}))
+  (keywordize-keys (into {} (mc/find-one "users" {:username username}))))
 
 (defn add-user [user]
   (if (is-valid/user? user)
-    (mc/insert "users" user)))
+    (mc/insert "users" (assoc user :password (hash-bcrypt (:password user))))))
 
 (defn get-phrase []
   "passwords are so completely last decade")
