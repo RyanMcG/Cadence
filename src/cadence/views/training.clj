42d7064d2c1dac23ebb815dbf94beeda113eb350
Ryan McGowan <ryan@ryanmcg.com>
2012-05-17 13:58:33 -0400

    Fleshes out the training page a bit more.

--- src/cadence/views/training.clj
+++ src/cadence/views/training.clj
@@ -7,23 +7,31 @@
         hiccup.core
         hiccup.page-helpers))
 
-(defpage training "/training" []
+(defpage training "/user/training" []
   (common/layout
     [:div.page-header
      [:h1 "Training"]]
     [:p "If you don't know what this is for please checkout the "
      (link-to "/#training" "blurb on the front page") "."]
     [:h2 "Let's get to training!"]
-    [:div.well
-     [:p.help "Simply type the following phrase in the form repeatedly until I
-              tell you to stop."]
-     [:h3.training-phrase (model/get-phrase)]
-     (let [train-count (sess/put! :train-count 0)]
-       [:form#trainer
-        (common/input {:type "text"
-                       :name (str "phrase-"
-                                  (sess/get :train-count))
-                       :placeholder ""})])]))
+    [:div#train_well.well.container-fluid
+     [:p.help "Simply type the following phrase in the form repeatedly until I"
+      " tell you to stop."]
+     (let [train-count (sess/put! :train-count 0)
+           phrase (model/get-phrase)]
+       (html [:div.row-fluid
+              [:div#training-phrase.input-xlarge.uneditable-input.span12
+               phrase]]
+             [:form#trainer.row-fluid
+              (common/input {:type "text"
+                             :eclass ".phrase.input-xlarge.span12"
+                             :name (str "phrase-"
+                                        (sess/get :train-count))
+                             :placeholder phrase})]))
+     [:div#feedback.row-fluid]
+     [:div#completion.row-fluid
+      [:h3.span2 "Completion: "]
+      [:div.progress.progress-success.span10 [:div.bar {:style "width: 0%;"}]]]]))
 
 (defpage post-training [:post "/training"] {:keys [cadence]}
   (resp/json cadence))
