fe0ee7d5fe0bf3c53d0d5407ba8d772abbf7ae68
Ryan McGowan <ryan@ryanmcg.com>
2012-05-20 17:46:07 -0400

    Makes flash/put! take a variable number of arguments.

--- src/cadence/model/flash.clj
+++ src/cadence/model/flash.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [get])
   (:require [noir.session :as sess]))
 
-(defn put! [t m]
+(defn put! [t & m]
   (sess/flash-put! {:type t :message m}))
 
 (def get sess/flash-get)
