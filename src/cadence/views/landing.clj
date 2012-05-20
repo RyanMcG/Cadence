a70d2e06ec5b001dc55a1157f466d55525dadc56
Ryan McGowan <ryan@ryanmcg.com>
2012-05-20 17:49:54 -0400

    Finishes signup and login routes and moves them to user.clj.

--- src/cadence/views/landing.clj
+++ src/cadence/views/landing.clj
@@ -1,72 +1,14 @@
 (ns cadence.views.landing
-  (:require [cadence.views.common :as common]
-            [cadence.model :as model]
-            [cadence.model.recaptcha :as recaptcha]
-            [cadence.model.validators :as is-valid]
-            [cadence.model.flash :as flash]
-            [cemerick.friend :as friend]
-            [noir.response :as resp])
+  (:require [cadence.views.common :as common])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
 
 (defpage root "/" []
   (common/layout
-    [:div.page-header [:h1 "Cadence"]]
+    [:div.page-header [:h1 "Welcome!"]]
     [:p "Welcome to cadence"]))
 
-(defpage login "/login" []
-  (common/layout
-    [:div.page-header [:h1 "Login"]]
-    (common/default-form
-      :#login.well.form-inline
-      {:action "/login" :method "POST"}
-      [{:type "username" :name "Username"}
-       {:type "password" :name "Password"}]
-      [{:value "Log In"}])))
-
-(defn- clear-identity [response]
-  (update-in response [:session] dissoc ::identity))
-
-(defpage logout "/logout" []
-  (flash/put! :success "You have been logged out.")
-  (clear-identity (resp/redirect "/")))
-
-(defpage signup "/signup" {:as user}
-  (common/layout
-    [:div.page-header [:h1 "Sign Up"]]
-    ; Make the recaptcha theme 'clean'
-    [:script "var RecaptchaOptions = { theme : 'clean'}"]
-    (common/control-group-form
-      :#login.well.form-horizontal
-      {:action "/signup" :method "POST"}
-      [{:type "username" :name "Username" :required "yes"
-        :value (escape-html (get user :username))}
-       {:type "text" :name "Name" :placeholder "Optional"
-        :value (escape-html (get user :name))}
-       {:type "email" :name "Email" :placeholder "Optional"
-        :value (escape-html (get user :email))}
-       {:type "password" :name "Password" :required "yes"
-        :value (get user :password)}
-       {:type "password" :name "Repeat Password"
-        :required "yes"}
-       {:type "custom" :name "Humans only"
-        :content (recaptcha/get-html
-                   (escape-html
-                     (get user :errors)))}]
-      [{:eclass :.btn-primary
-        :value "Sign Up"}])))
-
-(defpage signup-check [:post "/signup"] {:as user}
-  (if (is-valid/user? user)
-    (do
-      (model/add-user
-        (select-keys user [:username :name :email :password])
-      (flash/put! :success "Successfully Signed Up!")
-      (resp/redirect (url-for root)))
-    (do
-      (flash/put! :error "Sorry, but your input has some validation errors.")
-      (render signup user)))))
 
 (defpage about "/about" []
   (common/layout
--- src/cadence/views/user.clj
+++ src/cadence/views/user.clj
@@ -1,8 +1,11 @@
 (ns cadence.views.user
   (:require [cadence.views.common :as common]
-            [cadence.model.flash :as flash]
-            [cemerick.friend :as friend]
             [cadence.model :as m]
+            [cadence.model.flash :as flash]
+            [cadence.model.recaptcha :as recaptcha]
+            [cadence.model.validators :as is-valid]
+            [cemerick.friend :as friend]
+            [noir.validation :as vali]
             [noir.response :as resp])
   (:use noir.core
         hiccup.core
@@ -17,3 +20,62 @@
 
 (defpage user-profile-default "/user/profile" []
   (resp/redirect (url-for user-profile {:username (m/identity)})))
+
+(defpage login "/login" {:keys [login_failed username]}
+  (common/layout
+    [:div.page-header [:h1 "Login"]]
+    (common/default-form
+      :#login.well.form-inline
+      {:action "/login" :method "POST"}
+      [{:type "username" :name "Username" :params {:value username}}
+       {:type "password" :name "Password"}]
+      [{:value "Log In"}])
+    (when (= login_failed "Y")
+      (common/alert :error "Sorry!" "You used a bad username/password."))))
+
+(defn- clear-identity [response]
+  (update-in response [:session] dissoc ::identity))
+
+(defpage logout "/logout" []
+  (flash/put! :success "You have been logged out.")
+  (clear-identity (resp/redirect "/")))
+
+(defpage signup "/signup" {:as user}
+  (common/layout
+    [:div.page-header [:h1 "Sign Up"]]
+    ; Make the recaptcha theme 'clean'
+    [:script "var RecaptchaOptions = { theme : 'clean' };"]
+    (common/control-group-form
+      :#login.well.form-horizontal
+      {:action "/signup" :method "POST"}
+      [{:type "username" :name "Username" :required "yes"
+        :value (escape-html (get user :username))}
+       {:type "text" :name "Name" :placeholder "Optional"
+        :value (escape-html (get user :name))}
+       {:type "email" :name "Email" :placeholder "Optional"
+        :value (escape-html (get user :email))}
+       {:type "password" :name "Password" :required "yes"
+        :value (get user :password)}
+       {:type "password" :name "Repeat Password"
+        :required "yes"}
+       {:type "custom" :name "Humans only"
+        :content (recaptcha/get-html
+                   (escape-html
+                     (get user :errors)))}]
+      [{:eclass :.btn-primary
+        :value "Sign Up"}])))
+
+(defpage signup-check [:post "/signup"] {:as user}
+  (if (is-valid/user? user)
+    (try
+      (m/add-user
+        (select-keys user [:username :name :email :password]))
+      (flash/put! :success "You've signed up! Now try and login.")
+      (resp/redirect (url-for login))
+      (catch com.mongodb.MongoException e
+        (flash/put! :error "Error: " "Sorry, that username is already in use.")
+        (vali/set-error :username "Please try a different username.")
+        (render signup user)))
+    (do
+      (flash/put! :error "Sorry, but your input has some validation errors.")
+      (render signup user))))
