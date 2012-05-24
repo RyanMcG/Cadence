abaa8b31e63e1029cfa0714eeaa7040342755559
Ryan McGowan <ryan@ryanmcg.com>
2012-05-24 03:57:47 -0400

    Excludes jackson-core-asl in project.clj.

--- project.clj
+++ project.clj
@@ -6,18 +6,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-marginalia "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.2.2"
+                  :exclusions [org.codehaus.jackson/jackson-core-asl]]
                  [net.tanesha.recaptcha4j/recaptcha4j "0.0.7"]
                  [com.novemberain/monger "1.0.0-beta4"]
                  ;[clauth "1.0.0-rc1"]
                  [amalloy/ring-gzip-middleware "0.1.1"]
+                 ;[ring-middleware-format "0.1.1"]
                  [fuziontech/ring-json-params "0.2.0"]
                  [com.cemerick/friend "0.0.8"]
                  ;[com.ml/libsvm "2.89"]
                  ;[svm-clj "0.0.1-SNAPSHOT"]
                  ;[edu.berkely.compbio/jlibsvm "0.902"]
                  ;[clj-ml "0.0.3-SNAPSHOT"]
-                 [com.leadtune/clj-ml "0.2.1"]
-                 ;[ring-middleware-format "0.1.1"]
-                 ;[noir-cljs "0.2.5"]
-                 [noir "1.2.2"]]
+                 [com.leadtune/clj-ml "0.2.1"]]
   :main cadence.server)
