ef83fc518290f71ae8b575e6866cb9435b4b88fc
Ryan McGowan <ryan@ryanmcg.com>
2012-05-24 21:38:22 -0400

    Causes training to only display if a user has untrained phrases.

--- src/cadence/views/training.clj
+++ src/cadence/views/training.clj
@@ -17,36 +17,42 @@
      [:h1 "Training"]]
     [:p "If you don't know what this is for please checkout the "
      (link-to "/#training" "blurb on the front page") "."]
-    [:h2 "Let's get down to business!"]
-    [:div#train_well.well.container-fluid
-     [:p.help "Simply type the following phrase in the form repeatedly until I"
-      " tell you to stop."]
-     (let [phrase (model/get-phrase)]
-       (html [:div.row-fluid
-              [:div#training-phrase.input-xlarge.uneditable-input.span12
-               phrase]]
-             [:form#trainer.row-fluid
-              (common/input {:type "text"
-                             :eclass ".phrase.input-xlarge.span12"
-                             :name "phrase"
-                             :placeholder phrase})]))
-     [:div#feedback.row-fluid]
-     [:div#completion.row-fluid
-      (let [trcount (count (patrec/kept-cadences))]
-        (html
+    (let [phrase-doc (model/get-phrase (:_id (model/get-auth)) false)
+          phrase (:phrase phrase-doc)]
+      (if (nil? phrase-doc)
+        ; There are no phrases for which the current user has not already done
+        ; training.
+        (common/alert :info [:h2 "You are already trained!"]
+                      (html [:p "There are a limited number of
+                                phrases and you have done the training for all
+                                of them. Currently there is no support for
+                                redoing your training for a given phrase, but
+                                hopefully there will be soon."]) false)
+        ; Otherwise we give the user a form to train with.
+        (do (sess/put! :training-phrase phrase-doc)
+          (html
+            [:h2 "Let's get down to business!"]
+            [:div#train_well.well.container-fluid
+             [:p.help "Simply type the following phrase in the form repeatedly
+                      until I tell you to stop."]
+             (common/phrase-fields "trainer" phrase)
+             [:div#feedback.row-fluid]
+             [:div#completion.row-fluid
+              (let [trcount (count (patrec/kept-cadences))]
+                (html
 
-          [:div
-           {:class (str "progress progress-success progress-striped span10"
-                        (when (>= trcount @patrec/training-min) " active"))}
-           [:div.bar
-            {:style (str "width: "
-                         (min 100 (* 100.0 (/ trcount @patrec/training-min)))
-                         "%;")}]]
-          (if (>= trcount @patrec/training-min)
-            [:div.span2 [:a.btn.btn-large.btn-success
-             {:href "/user/profile"} "Already done!"]]
-            [:div.span2 [:a.btn.btn-large.disabled
-             {:href "#"} "Complete"]])))]]))
+                  [:div
+                   {:class (str "progress progress-success progress-striped span10"
+                                (when (>= trcount @patrec/training-min) " active"))}
+                   [:div.bar
+                    {:style (str "width: "
+                                 (min 100 (* 100.0 (/ trcount @patrec/training-min)))
+                                 "%;")}]]
+                  (if (>= trcount @patrec/training-min)
+                    [:div.span2 [:a.btn.btn-large.btn-success
+                                 {:href "/user/profile"} "Already done!"]]
+                    [:div.span2 [:a.btn.btn-large.disabled
+                                 {:href "#"} "Complete"]])))]]))))))
 
 (defpage post-training [:post "/user/training"] {:as unkeyed-cad}
   ; I like maps with keyword keys
