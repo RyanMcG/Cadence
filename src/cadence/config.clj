cf8b01402870068052075536a217396bbc1e00f8
Ryan McGowan <ryan@ryanmcg.com>
2012-05-31 03:10:39 -0400

    Adds comments to cadence.config.

--- src/cadence/config.clj
+++ src/cadence/config.clj
@@ -3,21 +3,30 @@
             [clojure.java.io :as io])
   (:import [java.net URL]))
 
+;; Define a map read from config.clj on the resource path.
 (def config-from-file
   (if-let [cres (io/resource "config.clj")]
     (-> cres (.getPath) (load-file)) {}))
 
-(defn read-config [config-var]
+(defn read-config
+  "Reads config by first accessing the map from the config file and falling back
+  on environment variables. This is heroku \"friendly\"."
+  [config-var]
   (get config-from-file
        (keyword (string/replace
                   (string/lower-case config-var)
                   "_" "-")) (System/getenv config-var)))
 
+;; Below we define some standard configuration values using read-config.
+
+;; These values are meant for setting up mongo.
 (def storage {:uri (read-config "MONGOHQ_URL")
               :db-name (read-config "MONGO_DB_NAME")
               :username (read-config "MONGO_USERNAME")
               :password (read-config "MONGO_PASSWORD")})
 
+;; Various tokens used by the application. At the moment Twitter is not being
+;; used, but hopefully it will be in the future.
 (def tokens
   {:recaptcha
    {:public-key (read-config "RECAPTCHA_PUBLIC_KEY")
