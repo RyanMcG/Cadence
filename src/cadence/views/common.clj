37c1abb65dcbfa09a389e4dba67a0a1e562c5300
Ryan McGowan <ryan@ryanmcg.com>
2012-06-02 18:56:04 -0400

    Adds google analytics code to base-layout.

--- src/cadence/views/common.clj
+++ src/cadence/views/common.clj
@@ -20,7 +20,19 @@
      (include-css "/css/bootstrap.min.css")
      (include-js (str "https://ajax.googleapis.com/ajax/libs/"
                       "jquery/1.7.2/jquery.min.js")
-                 "/js/jquery-1.7.2.min.js")]
+                 "/js/jquery-1.7.2.min.js")
+     [:script {:type "text/javascript"}
+      "var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-32354071-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
+      })();"]]
     [:body
      content]
     (include-js "/js/bootstrap.min.js")
