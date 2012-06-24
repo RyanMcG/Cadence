c5c24111b2d943092b64d655ac6ccaca3e26da05
Ryan McGowan <ryan@ryanmcg.com>
2012-06-24 16:17:46 -0700

    Fixes bug (didn't wrap let in html).

--- src/cadence/views/profile.clj
+++ src/cadence/views/profile.clj
@@ -1,4 +1,4 @@
-(ns cadence.views.user
+(ns cadence.views.profile
   (:require [cadence.views.common :as common]
             [cadence.model :as m]
             [cadence.model.flash :as flash]
@@ -11,41 +11,42 @@
         hiccup.page-helpers))
 
 (defpartial phrases []
-  "Phrases")
+  [:p "Phrases"])
 
 (defpartial visualizer []
-  "Phrases")
+  [:p "Visualizer"])
 
 (defpage user-profile "/user/profile/:username" {:keys [username]}
   (if (= username (m/identity))
     (common/with-javascripts (conj common/*javascripts* "/js/g.raphael-min.js")
       (common/layout
         (let [user (m/get-auth)]
-          ; When there are a suffecient number of training cadences adn the
-          (when (<= @patrec/training-min (count (patrec/kept-cadences)))
-            (let [user-id (:_id user)
-                  phrase-id (:_id (sess/get :training-phrase))]
-              ; Add cadences to mongo
-              (m/add-cadences user-id phrase-id (sess/get :training-cadences))
-              ; Add the current user-id to array of trained users on the given
-              ; phrase
-              (m/add-trained-user-to-phrase user-id phrase-id))
-            ; Remove the cadenences from training stored in the client's session.
-            (sess/remove! :training-cadences)
-            (sess/remove! :training-phrase)
-            ; Let the user know they've been a good minion ;-) .
-            (common/alert :success "Congratulations!"
-                          "You've sucessfully completed training!"))
-
-          [:div.page-header [:h1 (h username)]
-           [:img {:src (gravatar (h (:email user)))}]] 
-          [:div.container-fluid
-           [:p (:blurb user)]
-           [:div.row-fluid
-            [:div.span6
-             (phrases)]
-            [:div.span6
-             (visualizer)]]])))
+          (html
+            ; When there are a suffecient number of training cadences
+            (when (<= @patrec/training-min (count (patrec/kept-cadences)))
+              (let [user-id (:_id user)
+                    phrase-id (:_id (sess/get :training-phrase))]
+                ; Add cadences to mongo
+                (m/add-cadences user-id phrase-id (sess/get :training-cadences))
+                ; Add the current user-id to array of trained users on the given
+                ; phrase
+                (m/add-trained-user-to-phrase user-id phrase-id))
+              ; Remove the cadenences from training stored in the client's
+              ; session.
+              (sess/remove! :training-cadences)
+              (sess/remove! :training-phrase)
+              ; Let the user know they've been a good minion ;-) .
+              (common/alert :success "Congratulations!"
+                            "You've sucessfully completed training!"))
+            [:div.page-header.username [:h1 (h (or (:name user) username))]
+             [:img {:src (gravatar (h (:email user))) :title (h username)}]]
+            [:div#profile.container-fluid
+             [:p (str user)]
+             [:div.row-fluid
+              [:div.span6
+               (phrases)]
+              [:div.span6
+               (visualizer)]]]))))
     (do
       (flash/put! :error (str "You cannot access " username "'s page."))
       (resp/redirect (str "/user/profile/" (m/identity))))))
