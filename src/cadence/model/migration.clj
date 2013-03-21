18a7cf3b6e7b246378c80a279a3a6745bf8a5900
Ryan McGowan <ryan@ryanmcg.com>
2013-03-20 21:02:46 -0400

    Rewrite migration library.

--- src/cadence/model/migration.clj
+++ src/cadence/model/migration.clj
@@ -1,7 +1,9 @@
 (ns cadence.model.migration
   (:require (cadence [config :refer [storage]]
-                     [model :as model])
-            [ragtime.core :as rag]
+                     [model :as model]
+                     [state :as state])
+            [ragtime [core :as rag]
+                     [strategy :as strat]]
             (monger [core :as mo]
                     [collection :as mc]
                     [operators :refer :all])
@@ -17,39 +19,26 @@
 
 (def migrations (atom []))
 
-(defn migration [id up down]
-  {:id (ObjectId. id)
-   :up (fn [db] (mo/with-db db (eval up)))
-   :down (fn [db] (mo/with-db db (eval down)))})
-
-(defmacro add-migrations
-  "Take in an arbiraty number of migrations where each is a syntax quoted form
-  with three root elements. Apply the migration function to each of these
-  forms. Each form should look something like:
-
-       `(\"somemongoobjectidhash\"
-           (up form)
-           (down form))
-  "
-  [& more-migrations]
+(defmacro defmigration [doc-string id up down]
+  "Define migrations as easily as possible."
   `(swap! migrations
-          concat
-          (map (partial apply migration)
-               [~@more-migrations])))
+          conj
+          ^{:doc ~doc-string}
+          {:id (ObjectId. ~id)
+           :up (fn [db#] (mo/with-db db# ~up))
+           :down (fn [db#] (mo/with-db db# ~down))}))
 
-(defn str-id
-  "Helper function for generating an id when adding migrations."
-  [] (str (ObjectId.)))
-
-;; Actually defined some migrations.
-(add-migrations
-  `("5108749844ae8febda9c2ed4"
-     (mc/update "users" {} {$set {:roles [:user]}} :multi true)
-     (mc/update "users" {} {$unset {:roles ""}} :multi true)))
+(state/compute)
+(def strategy
+  "Pick a conflict handling strategy based on the state of the currently running
+  server."
+  (if (state/development?)
+    strat/rebase
+    strat/raise-error))
 
 (defn run-migrations
   "Run defined migrations."
   ([db]
    (connect-if-necessary)
-   (rag/migrate-all db @migrations))
+   (rag/migrate-all db @migrations strategy))
   ([] (run-migrations mo/*mongodb-database*)))
