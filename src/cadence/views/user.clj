b8527b8e1c4325c2592e5dc23eaf95cf7f21f04f
Ryan McGowan <ryan@ryanmcg.com>
2013-03-21 22:09:39 -0400

    Improve migrations listing.

--- src/cadence/views/user.clj
+++ src/cadence/views/user.clj
@@ -8,10 +8,13 @@
             [cadence.pattern-recognition :as patrec]
             (ragtime [core :as rag])
             [ring.util.anti-forgery :refer [anti-forgery-field]]
+            (clj-time [coerce :as time-coerce]
+                      [format :as time-format])
             [noir.validation :as vali]
             [noir.session :as sess]
             [noir.response :as resp])
-  (:use (hiccup page def element util)))
+  (:use (hiccup core page def element util))
+  (:import (org.bson.types ObjectId)))
 
 (defn profile [{{:keys [username]} :route-params :as request}]
   (if (= username (m/identity))
@@ -131,7 +134,42 @@
       ; and then load the page again.
       (signup user))))
 
-(defn migrations [request]
+(defhtml migration-row
+  "Convert a modified migration map to a table row."
+  [{:keys [id doc applied?]}]
+  [:tr
+   [:td.date (h (str (time-format/unparse
+                         (:rfc822 time-format/formatters)
+                         (time-coerce/from-long
+                           (.getTime (ObjectId. id))))))]
+   [:td.id [:code (h id)]]
+   [:td.doc [:p (h doc)]]
+   (let [badge-class (if applied? "label-success" "")
+         badge-icon (if applied? "icon-ok-sign" "icon-remove-sign")
+         badge-text (if applied? "Applied" "Not Applied")
+         button-class (if applied? "btn-inverse" "btn-primary")
+         button-text (if applied? "Rollback" "Apply")]
+     (html
+       [:td.applied [:span {:class (str "label " badge-class)}
+                     [:i {:class (str "icon-white " badge-icon)}]
+                     (str " " badge-text)]]
+
+       [:td.controls [:button {:data-object-id id
+                               :class (str "btn " button-class)}
+                      button-text]]))])
+
+(defn migrations
+  "A nice place to view migrations."
+  [request]
   (common/layout
     [:h2 "Migrations"]
-    [:pre [:code (escape-html (seq (migration/list-migrations)))]]))
+    [:table#migrations.table.table-striped.table-bordered
+     [:thead
+      [:tr
+       [:th.date "Timestamp"]
+       [:th.id "ObjectId"]
+       [:th.doc "Description"]
+       [:th.applied "Applied?"]
+       [:th.controls]]]
+     [:tbody
+      (map migration-row (migration/list-migrations))]]))
