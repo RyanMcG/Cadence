e674f9a37c7003fcf898cc82aaa5f9b35e390fbf
Ryan McGowan <ryan@ryanmcg.com>
2013-03-30 11:52:13 -0400

    Update http-kit, friend and lib-noir.

--- project.clj
+++ project.clj
@@ -9,12 +9,13 @@
                           :mirrors {#"central|clojars"
                                     "http://s3pository.herokuapp.com/clojure"}}}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.3.5"]
+                 [com.cemerick/friend "0.1.5"]
+                 [lib-noir "0.4.6"]
                  [ragtime/ragtime.core "0.3.2"]
                  [org.clojars.ryanmcg/ring-anti-forgery "0.3.1-SNAPSHOT"]
                  [compojure "1.1.3"]
                  [hiccup "1.0.2"]
-                 [http-kit "2.0.0-RC4"]
+                 [http-kit "2.0.0"]
                  [ring-refresh "0.1.1"]
                  [bultitude "0.1.7"]
                  [com.cemerick/drawbridge "0.0.6"]
@@ -22,6 +23,5 @@
                  [com.novemberain/monger "1.4.2"]
                  [amalloy/ring-gzip-middleware "0.1.1"]
                  [ring-middleware-format "0.1.1"]
-                 [com.cemerick/friend "0.1.3"]
                  [com.leadtune/clj-ml "0.2.4"]]
   :main cadence.server)
