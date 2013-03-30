36213efd42d316f52b7d4c109ef3ae6240a7b7d1
Ryan McGowan <ryan@ryanmcg.com>
2013-03-30 12:05:11 -0400

    Add another migration for demo purposes.

--- src/cadence/migrations.clj
+++ src/cadence/migrations.clj
@@ -16,3 +16,8 @@
   (do (mc/update "users" {} {$set {:roles [:user]}} :multi true)
       (mc/update "users" {:username "RyanMcG"} {$push {:roles :admin}}))
   (mc/update "users" {} {$unset {:roles ""}} :multi true))
+
+(defmigration "Change my name (just for demo)"
+  "51571b7244aedb2065e2a3d7"
+  (mc/update "users" {:username "RyanMcG"} {$set {:name "Ryan Vincent McGowan"}})
+  (mc/update "users" {:username "RyanMcG"} {$set {:name "Ryan McGowan"}}))
