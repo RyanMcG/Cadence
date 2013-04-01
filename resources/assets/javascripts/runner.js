a78d87144fb7241ce07180787e28308efdeb4aaa
Ryan McGowan <ryan@ryanmcg.com>
2013-04-01 01:06:04 -0400

    Correctly post data as JSON in runner.js.

--- resources/assets/javascripts/runner.js
+++ resources/assets/javascripts/runner.js
@@ -14,20 +14,36 @@ jQuery(function ($) {
     return alert;
   };
 
+  var handleErrors = function (jqXHR) {
+    if (jqXHR.status >= 400 && jqXHR.status < 500) {
+      var data = JSON.parse(jqXHR.responseText);
+      $.each(data.errors, function(index, value) {
+        $feedback.append(generateAlert("alert-error", value));
+      });
+    } else {
+      $feedback.clearAlerts();
+      $feedback.append(
+        generateAlert("alert-error",
+                      "<strong>Uh oh!</strong> It appears an error occurred on my side.")
+      );
+    }
+  };
+
   $("form#trainer").cadence(function (result) {
-    $.post("/user/training", result, function (data, textStatus, jqXHR) {
+    $.ajax({
+      url: '/user/training',
+      type: 'POST',
+      data: JSON.stringify(result),
+      contentType: 'application/json; charset=UTF-8'
+    }).done(function (data, textStatus, jqXHR) {
       // Always clear alerts whenever we get new feedback from the server.
       $feedback.clearAlerts();
 
-      if (!data.success) {
-        $feedback.append( generateAlert("alert-error",
-                        "It appears that the supplied cadence result was invalid."));
-      } else if (data.progress === 0) {
+      if (data.progress === 0) {
         $feedback.append(generateAlert("alert-warning",
                         "<p>That cadence you entered didn't really help with training.</p>" +
                           "<p>Did you type in the given phrase differently from before?</p>"));
-      }
-      if (data.done) {
+      } else if (data.progress >= 100) {
         $feedback.append(
           generateAlert("alert-success",
                         "<p>Yay! You've done enough training. Feel free to continue " +
@@ -43,6 +59,7 @@ jQuery(function ($) {
         $cButton.parent().removeClass("span2").addClass("span4");
         $cButton.attr("href", "/user/profile");
       }
+
       // Modify the size of the progress bar.
       var progressBarWidth = data.progress;
       if (progressBarWidth > 100) {
@@ -51,40 +68,25 @@ jQuery(function ($) {
         progressBarWidth = 0;
       }
       progressBar.css("width", (progressBarWidth + "%"));
-    }, 'json')
-    .fail(function () {
-      $feedback.clearAlerts();
-      $feedback.append(
-        generateAlert("alert-error",
-                      "<strong>Uh oh!</strong> It appears an error occurred on my side.")
-      );
-    });
+    }).fail(handleErrors);
   });
 
   // Authenticate
   $("form#authenticate").cadence(function (result) {
-    $.post("/user/auth", result, function (data, textStatus, jqXHR) {
+    $.ajax({
+      url: '/user/auth',
+      type: 'POST',
+      data: JSON.stringify(result),
+      contentType: 'application/json; charset=UTF-8'
+    }).done(function (data, textStatus, jqXHR) {
       // Always clear alerts whenever we get new feedback from the server.
       $feedback.clearAlerts();
-      if (data.success) {
-        $feedback.append(generateAlert("alert-" + data.type, data.message));
-        $feedback.append(
-          generateAlert("alert-info",
-                        "<h4>Last Result:</h4>" +
-                          "<pre>" + JSON.stringify(data, null, "   ") + "</pre>")
-        );
-      } else {
-        $.each(data.errors, function(index, value) {
-          $feedback.append(generateAlert("alert-error", value));
-        });
-      }
-    }, 'json')
-    .fail(function () {
-      $feedback.clearAlerts();
+      $feedback.append(generateAlert("alert-" + data.type, data.message));
       $feedback.append(
-        generateAlert("alert-error",
-                      "<strong>Uh oh!</strong> It appears an error occurred on my side.")
+        generateAlert("alert-info",
+                      "<h4>Last Result:</h4>" +
+                        "<pre>" + JSON.stringify(data, null, "   ") + "</pre>")
       );
-    });
+    }).fail(handleErrors);
   });
 });
