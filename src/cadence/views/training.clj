cc4de343c1a3f3a019714f1220f6fcb65f7fc5c5
Ryan McGowan <ryan@ryanmcg.com>
2013-04-16 00:12:09 -0400

    Rewrite training and training-post.

--- src/cadence/views/training.clj
+++ src/cadence/views/training.clj
@@ -8,93 +8,69 @@
             (noir [response :as resp]
                   [session :as sess]))
   (:use clojure.walk
-        (hiccup core page element)))
+        (hiccup def core page element)))
 
-(defn training [request]
+
+(defhtml training-well
+  "Content/markup for the training page."
+  [phrase done percent-complete]
+  [:h2 "Let's get down to business!"]
+  [:div#train_well.well.container-fluid
+   [:p.help "Simply type the following phrase in the form repeatedly
+            until I tell you to stop."]
+   ; Show a phrase to type in and an input field to for the user to
+   ; copy it.
+   (common/phrase-fields "trainer" phrase)
+   [:div#feedback.row-fluid]
+   [:div#completion.row-fluid
+    [:div
+     {:class (str "progress progress-success progress-striped
+                  span10" (when done " active"))}
+     [:div.bar
+      {:style (str "width: " percent-complete "%;")}]]
+    [:div.span2 [:a {:href "#" :class (str "btn btn-large disabled"
+                                           (when done " btn-success"))}
+                 "Complete"]]]])
+
+(defn training
+  "Determine what phrase the current user should train on and display some
+  elements to input the phrase and see some feedback and progress."
+  [request]
   (common/layout
     [:div.page-header
      [:h1 "Training"]]
     [:p "If you don't know what this is for please checkout the "
      (link-to "/#training" "blurb on the front page") "."]
-    (if-let [phrase-doc (or (sess/get :training-phrase)
-                            (get (sess/put!
-                                   :possible-training-phrase
-                                   (model/get-phrase-for-training
-                                     (:_id (model/get-auth))))
-                                 :possible-training-phrase))]
-      ; Found a training phrase in the session or grabbed a new one from the
-      ; database.
-      (let [phrase (:phrase phrase-doc)]
-        (html
-          [:h2 "Let's get down to business!"]
-          [:div#train_well.well.container-fluid
-           [:p.help "Simply type the following phrase in the form repeatedly
-                    until I tell you to stop."]
-           ; Show a phrase to type in and an input field to for the user to
-           ; copy it.
-           (common/phrase-fields "trainer" phrase)
-           [:div#feedback.row-fluid]
-           [:div#completion.row-fluid
-            (let [trcount (count (patrec/kept-cadences))]
-              (html
-                [:div
-                 {:class (str "progress progress-success progress-striped
-                              span10"
-                              (when (>= trcount @patrec/training-min)
-                                " active"))}
-                 [:div.bar
-                  {:style (str "width: "
-                               (min 100
-                                    (* 100.0
-                                       (/ trcount @patrec/training-min)))
-                               "%;")}]]
-                (if (>= trcount @patrec/training-min)
-                  [:div.span2 [:a.btn.btn-large.btn-success
-                               {:href "/user/profile"} "Already done!"]]
-                  [:div.span2 [:a.btn.btn-large.disabled
-                               {:href "#"} "Complete"]])))]]))
-      ; If there is no training-phrase in the session and the current user has
-      ; no untrained phrases then the user cannot do more training.
-      (common/alert :info [:h2 "You are already trained!"]
-                    (html [:p "There is a limited number of phrases and you
-                              have done the training for all of them."]
-                          [:p "Currently, there is no support for redoing your
-                              training for a given phrase, but hopefully there
-                              will be soon."]) false))))
+    (let [current-user-id (:_id (model/current-user))
+          phrase-from-session (sess/get :training-phrase)
+          phrase-doc (if (or (nil? phrase-from-session)
+                             (model/phrase-complete-for-user?
+                               (:_id phrase-from-session)
+                               current-user-id))
+                       (model/get-phrase-for-training current-user-id)
+                       phrase-from-session)
+          {phrase-id :_id phrase :phrase} phrase-doc
+          training-count (model/count-cadences phrase-id current-user-id)
+          training-min @patrec/training-min
+          done (>= training-count training-min)
+          percent-complete (min 100 (* 100.0 (/ training-count training-min)))]
+      (sess/put! :training-phrase phrase-doc)
+      (training-well phrase done percent-complete))))
 
-(defn training-post [{unkeyed-cad :body-params}]
-  ; I like maps with keyword keys
-  (let [cadence (keywordize-keys unkeyed-cad)]
-    ; When training-phrase is unset, set it with the value of
-    ; possible-training-phrase (set when /user/training is accessed via GET). If
-    ; the client somehow manipulates this the worst case scenario is that
-    ; :training-phrase will be null which should be caught by the cadence?
-    ; validator.
-    (when (nil? (sess/get :training-phrase))
-      (sess/put! :training-phrase (let [tp (sess/get :possible-training-phrase)]
-                                    (sess/remove! :possible-training-phrase)
-                                    tp)))
-    ; Ensure that the supplied data is really what I want it to be and not some
-    ; glitch or fabrication. (This uses noir.validation)
-    (if (is-valid/cadence? cadence false)
-      (if (patrec/keep-cadence cadence)
-        (let [trnmin @patrec/training-min
-              kept (patrec/kept-cadences)
-              trcount (count kept)
-              done (>= trcount trnmin)]
-          (resp/json {:success true
-                      :done done
-                      :session kept
-                      :progress (* 100.0 (/ trcount trnmin))}))
-        (resp/json {:success true
-                    :done false
-                    :progress 0}))
-      ; If bad data was received then tell the client we were not successful
-      (resp/status 400 (resp/json {:success false
-                                   ; Grab errors put on the cadence field using
-                                   ; noir.validation
-                                   :errors (vali/get-errors :cadence)
-                                   :progress 0})))))
+(defn training-post
+  "Add the posted cadence to the current user if it is valid and helps with
+  training."
+  [{unkeyed-cad :body-params}]
+  (let [cadence (keywordize-keys unkeyed-cad)
+        phrase-doc (sess/get :training-phrase)]
+    (if (is-valid/cadence-for? :training cadence)
+      (let [training-min @patrec/training-min
+            kept-count (model/keep-cadence (:_id phrase-doc)
+                                           (:_id (model/current-user))
+                                           cadence)]
+        (resp/json {:done (>= kept-count training-min)
+                    :progress (* 100.0 (/ kept-count training-min))}))
+      (resp/status 400 (resp/json {:errors (vali/get-errors :cadence)})))))
 
 (defn auth [{:keys [as-user]}]
   (common/layout
