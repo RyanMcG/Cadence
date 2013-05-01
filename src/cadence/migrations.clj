5baab23b4e3ff13e0e7e13658af311f0f251ab67
Ryan McGowan <ryan@ryanmcg.com>
2013-05-01 01:30:47 -0400

    Add migration to turn all user_id's and phrase_id's in cadences collection into ObjectIds.

--- src/cadence/migrations.clj
+++ src/cadence/migrations.clj
@@ -22,3 +22,14 @@
   "51571b7244aedb2065e2a3d7"
   (mc/update "users" {:username "RyanMcG"} {$set {:name "Ryan Vincent McGowan"}})
   (mc/update "users" {:username "RyanMcG"} {$set {:name "Ryan McGowan"}}))
+
+(defmigration "Make ObjectId's in cadences actually objectIds"
+  "518081b644aedb9b3d161544"
+  (doseq [doc (mc/find-maps "cadences" {} [:phrase_id :user_id])]
+    (let [{:keys [phrase_id user_id]} doc]
+      (mc/save "cadences" (merge doc {:phrase_id (to-object-id (str phrase_id))
+                                      :user_id (to-object-id (str user_id))}))))
+  (doseq [doc (mc/find-maps "cadences" {} [:phrase_id :user_id])]
+    (let [{:keys [phrase_id user_id]} doc]
+      (mc/save "cadences" (merge doc {:phrase_id (str phrase_id)
+                                      :user_id (str user_id)})))))
