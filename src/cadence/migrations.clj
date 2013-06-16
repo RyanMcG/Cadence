7d668eda613025ec620912abc5c3b4d53c007b11
Ryan McGowan <ryan@ryanmcg.com>
2013-06-16 16:23:35 -0400

    Add migration for moving completed phrases from phrases to users.

--- src/cadence/migrations.clj
+++ src/cadence/migrations.clj
@@ -1,6 +1,7 @@
 (ns cadence.migrations
   "Define some migrations yo."
   (:require [cadence.model.migration :refer [defmigration]]
+            [cadence.model :as model]
             (monger [collection :as mc]
                     [conversion :refer [to-object-id]]
                     [operators :refer :all]))
@@ -31,3 +32,19 @@
   (doseq [{:keys [phrase_id user_id] id :_id} (mc/find-maps "cadences" {} [:phrase_id :user_id])]
     (mc/update-by-id "cadences" id {$set {:phrase_id (str phrase_id)
                                           :user_id (str user_id)}})))
+
+(defn- phrase-ids-completed-by-user
+  "Get phrase ids of phrases that have been completed by the given user"
+  [user-id]
+  (filter #(model/phrase-complete-for-user? % user-id)
+          (map :_id (mc/find-maps "phrases" {} [:_id]))))
+
+(defmigration "Move completed phrases for users from phrases coll to users"
+  "51b6a20544ae3f194920a4c4"
+  (do
+    (doseq [{user-id :_id} (mc/find-maps "users" {} [:_id])]
+      (mc/update-by-id "users"
+                       user-id
+                       {$set {:phrases
+                              (phrase-ids-completed-by-user user-id)}}))
+    (mc/update "phrases" {} {$unset {:phrases ""}} :multi true)))
